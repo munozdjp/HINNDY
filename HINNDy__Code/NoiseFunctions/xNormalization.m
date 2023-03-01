a5861a79293740dab7abd21a6199e369000c90b3
JuanDiaz <67231886+munozdjp@users.noreply.github.com>
2023-03-01 14:10:00 +0300

    Delete HINNDy__Code directory

--- HINNDy__Code/NoiseFunctions/xNormalization.m
+++
@@ -1,9 +0,0 @@
-function [x,yzero]=xNormalization(x,yzero)
-    xMaximum = max(abs(x));
-    yzeroMaximum = max(abs(yzero));
-    xNormalized = x./xMaximum;
-    x = xNormalized;
-%     yzeroNormalized = yzero./yzeroMaximum;
-%     yzero = yzeroNormalized;
-        yzero = yzero;
-end 
